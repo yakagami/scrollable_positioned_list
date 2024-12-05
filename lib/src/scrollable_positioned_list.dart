f3afff80de0247a4f29d574f27bf143c21de90f6
yakagami <92773903+yakagami@users.noreply.github.com>
2024-12-05 14:02:36 -0500

    expose offset

--- lib/src/scrollable_positioned_list.dart
+++ lib/src/scrollable_positioned_list.dart
@@ -289,6 +289,8 @@ class ScrollOffsetController {
   /// ending offset.
   ScrollPosition get position => _scrollableListState!.primary.scrollController.position;
 
+  double get offset => _scrollableListState!.primary.scrollController.offset;
+
   Future<void> animateScroll(
       {required double offset,
       required Duration duration,
